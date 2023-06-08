3bebb2a64766e34304348c13446e0ebf42d4f09e
juanmzabala <juanmzabala@gmail.com>
2023-06-08 16:43:42 -0300

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "juanmzabala.github.io",
   server: {
-    host: true
+    host: false
   }
 })
